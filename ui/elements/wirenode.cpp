94ced41030f987da124ff3cbe76067c71e95a145
Jonathan Heathcote <mail@jhnet.co.uk>
2010-08-25 20:32:47 +0100

    Made selection of wire nodes work for othogonal lines and also work more intuatively. Selection drawing is currently incorrect.

--- ui/elements/wirenode.cpp
+++ ui/elements/wirenode.cpp
@@ -1,5 +1,9 @@
 #include "ui/elements/wirenode.h"
 
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+
 using namespace gator::ui;
 
 WireNode::WireNode(int x, int y) :
@@ -13,6 +17,22 @@ WireNode::~WireNode(void)
 {
 	// Do nothing...
 } // WireNode::~WireNode
+
+
+bool
+WireNode::HasBlob(void)
+{
+	int num_connections = input != NULL ? 1 : 0;
+	
+	std::vector<WireNode*>::iterator node;
+	for (node = outputs.begin(); node != outputs.end(); ++node) {
+		if (++num_connections > 2) {
+			return true;
+		}
+	}
+	
+	return false;
+} // WireNode::HasBlob
 
 
 bool
@@ -37,22 +57,11 @@ WireNode::Draw(Canvas *canvas)
 		          canvas->XS(input->x), canvas->YS(input->y), colour);
 	}
 	
-	int num_outputs = 0;
-	std::vector<WireNode*>::iterator node;
-	for (node = outputs.begin(); node != outputs.end(); ++node) {
-		WireNode *output = (*node);
-		if (++num_outputs > 1) {
-			filledCircleColor(surf, x, y, canvas->GetScale()/2, colour);
-			break;
-		}
-	}
+	if (HasBlob())
+		filledCircleColor(surf, x, y, canvas->GetScale()/2, colour);
 	
 	return true;
 } // WireNode::Draw
-
-
-#define MIN(x, y) ((x) < (y) ? (x) : (y))
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 
 int
@@ -127,23 +136,85 @@ WireNode::DisconnectOutput(WireNode *output)
 bool
 WireNode::IsAtPoint(int x, int y)
 {
-	if (input == NULL)
-		return false;
-	
-	int l = (input->GetX() < this->GetX()) ? input->GetX() : this->GetX();
-	int r = (input->GetX() > this->GetX()) ? input->GetX() : this->GetX();
-	int t = (input->GetY() < this->GetY()) ? input->GetY() : this->GetY();
-	int b = (input->GetY() > this->GetY()) ? input->GetY() : this->GetY();
-	
-	if (x < l || x > r || y < t || y > b) {
-		printf("no: outside rect %d, %d: l%d, r%d, t%d, b%d\n", x, y, l,r,t,b);
-		return false;
-	}
-	
-	if (l == r || t == b) {
-		printf("yes: 0 W/H\n");
+	if (IsPointOnInputLine(x,y))
 		return true;
+	
+	if (input != NULL
+	    && !input->IsNodeAtPoint(x,y)
+	    &&  input->IsPointOnInputLine(x,y))
+		return true;
+	
+	std::vector<WireNode*>::iterator node;
+	for (node = outputs.begin(); node != outputs.end(); ++node) {
+		WireNode *output = (*node);
+		
+		if (!output->IsNodeAtPoint(x,y) && output->IsPointOnInputLine(x,y))
+			return true;
 	}
 	
 	return false;
 } // WireNode::IsAtPoint
+
+
+bool
+WireNode::IsPointOnInputLine(int x, int y)
+{
+	if (input == NULL)
+		return false;
+	
+	int l = GetX();
+	int r = l + GetWidth();
+	int t = GetY();
+	int b = t + GetHeight();
+	
+	// Outside the rectangle this wire segment occupies
+	if (x < l || x > r || y < t || y > b) {
+		return false;
+	}
+	
+	// This wire is orthogonal
+	if (l == r || t == b) {
+		return true;
+	}
+	
+	
+	// The wire is not orthogonal
+	
+	// Not on an end point, find the distance of the point from the line
+	// p1: This point
+	double p1x = (double) this->x;
+	double p1y = (double) this->y;
+	
+	// p2: The point at the other end of the line
+	double p2x = (double) input->x;
+	double p2y = (double) input->y;
+	
+	// p3: The point where the user clicked
+	double p3x = (double) x;
+	double p3y = (double) y;
+	
+	double q = (p2y - p1y) / (p2x - p1x);
+	
+	// For equations in the form y = bx + a these are the values of a for
+	//   l1: The line between p1 and p2
+	//   l2: The line perpendicular to l1 going through p3
+	double l1a = p1y - (q * p1x);
+	double l2a = p3y + (p3x / q);
+	
+	// p4: The point of intersection between l1 and l2
+	double p4x = (l2a - l1a) / (q + (1.0/q));
+	double p4y = (q * p4x) + p1y - (q * p1x);
+	
+	if ((pow(p3x - p4x, 2) + pow(p3y - p4y, 2)) <= 0.5)
+		return true;
+	
+	return false;
+} // WireNode::IsPointOnInputLine
+
+
+bool
+WireNode::IsNodeAtPoint(int x, int y)
+{
+	return (this->x == x && this->y == y);
+} // WireNode::IsNodeAtPoint
+
